71f32a76832ef56ab851fb0499f425f58760eb88
Jesús Paredes <jjesus.paredesl@gmail.com>
2023-01-16 11:47:17 -0300

    favicon

--- src/Components/Experienciacofi.jsx
+++ src/Components/Experienciacofi.jsx
@@ -38,7 +38,7 @@ const Experienciacofi = ({ mensaje }) => {
 
           </div>
 
-          <div className="row bg-experiencia py-3 mt-3 align-items-center">
+          <div className="row bg-experiencia py-3 mt-3 align-items-center rounded-3">
             <div className="col-md-6 bg-experiencia rounded-3 ">
               <h3 className="text-center mt-5">Delivery</h3>
               <p className="my-5 text-center fs-5">Los clientes pueden pedir sus bebidas y productos favoritos donde sea que estén con operadores logísticos al servicio como Pedidos Ya.</p>
@@ -47,19 +47,19 @@ const Experienciacofi = ({ mensaje }) => {
 
 
                 <Link to={"/category"}>
-                  
-                <button className="rounded-3 btn btn-success fw-bold border-0">Escoge tu producto y te lo llevamos </button>
 
-                      </Link>
+                  <button className="rounded-3 btn btn-success fw-bold border-0">Escoge tu producto y te lo llevamos </button>
 
+                </Link>
+
+              </div>
             </div>
-          </div>
-          <div className="col-md-6 ">
-            <img className="img-fluid  mx-auto rounded-3" src="/image/Delivery.png" alt="Delivery" />
+            <div className="col-md-6 ">
+              <img className="img-fluid  mx-auto rounded-3" src="/image/Delivery.png" alt="Delivery" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div >
   );
 };
