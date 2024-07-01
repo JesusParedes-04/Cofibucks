f58f47eed716f8828027d90b97df643d112afaca
Jesus <jjesus.paredes@gmail.com>
2024-07-01 12:11:37 -0300

    cofibucks-V-0.0.2-styles-responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,13 +9,24 @@ const Footer = () => {
             <div className="row footer-class mt-5">
                 <div className="col d-flex align-items-center justify-content-center py-4">
                     <span>
-                        <i className="fa-brands fa-facebook ms-2 fs-4 text-light"></i>
+
+                        <a href="https://web.facebook.com/StarbucksCba">
+                            <i className="fa-brands fa-facebook ms-2 fs-4 text-light">  </i>
+                        </a>
+
                     </span>
                     <span>
-                        <i className="fa-brands fa-instagram ms-2 fs-4 text-light"></i>
+
+                        <a href="https://www.instagram.com/starbucksargentina/ ">    
+                         <i className="fa-brands fa-instagram ms-2 fs-4 text-light">
+                        </i></a>
+
+
                     </span>
                     <span>
-                        <i className="fa-brands fa-linkedin ms-2 fs-4 text-light"></i>
+                        <a href="https://www.linkedin.com/company/starbucks/">
+                        <i className="fa-brands fa-linkedin ms-2 fs-4 text-light"></i></a>
+                    
                     </span>
                 </div>
             </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,46 +3,58 @@ import home_img from '../../public/4365502.jpg'
 import coffeHome from '/vecteezy_cappuccino-coffee-cup-cutout_23522858.png'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { Link } from "react-router-dom";
 AOS.init();
 
 
 const Home = () => {
   return (
     <div className="container">
-      <div className="row main-heading">
-        <div className="col-md-6 position-relative">
-          <h2 className="maintext position-absolute">Bienvenidos</h2>
-          <h3 className="subtitle position-absolute tittle-products">
-            Tu viaje al mejor café comienza aquí.
-          </h3>
-        </div>
-        <div className="col-md-6">
-          <img src={coffeHome} alt="Café" className="img-fluid" />
-        </div>
+      <div className="container-title">
+        <h2 className="maintext">Bienvenidos</h2>
       </div>
 
-      <div data-aos="fade-down">
-      <div className="row">
-        <div className="col-md-6 alert-success rounded-2" role="alert">
-          <h2 className="text-uppercase text-center mt-5">Nosotros</h2>
-          <h3 className="text-center">La tiendita del buen café</h3>
-          <p className="pt-5 mx-5">
-            En Cofibucks, creemos que cada taza de café es una invitación a un viaje sensorial. Desde el año 1970, hemos dedicado nuestro corazón y alma a la búsqueda de los granos de café más exquisitos, llevando a nuestros clientes en una aventura de sabores que trasciende fronteras.
-          </p>
-          <p className="mx-5">
-            Nuestra historia comenzó en las laderas de las montañas de Colombia, donde descubrimos nuestra pasión por el café artesanal. Con cada grano seleccionado a mano y cada lote tostado a la perfección, nos comprometemos a ofrecer una experiencia única que solo un café de calidad superior puede proporcionar.
-          </p>
-          <p className="mx-5">
-            Te invitamos a visitarnos y ser parte de nuestra comunidad. Ya sea que prefieras un espresso intenso, un capuchino cremoso o una de nuestras especialidades de la casa, estamos aquí para servirte con una sonrisa y el mejor café que hayas probado.
-            ¡Te esperamos en Café Aromas del Mundo, donde cada sorbo cuenta una historia!
-          </p>
-        </div>
-        <div className="col-md-6">
-          <img className="img-fluid rounded-3" src={home_img} alt="Imagen home" />
-        </div>
+      <div className="container-coffee mb-3">
+        <h3 className="subtitle tittle-products">
+          Tu viaje al mejor café comienza con nosotros.
+        </h3>
+
       </div>
-    </div>
-    </div>
+
+        <div className="container-cols">
+
+<Link to={"/experiencia/"} className="button-main">Drive thru
+</Link>
+
+<Link to={"/category/shaken-expresso"} className="button-main">Nuevos Sabores
+</Link>
+
+</div>
+ 
+
+        <div data-aos="fade-down">
+          <div className="row mt-5">
+
+            <div className="col-md-6 alert-success rounded-2" role="alert">
+              <h2 className="text-uppercase text-center mt-5">Nosotros</h2>
+              <h3 className="text-center">La tiendita del buen café</h3>
+              <p className="pt-5 mx-5">
+                En Cofibucks, creemos que cada taza de café es una invitación a un viaje sensorial. Desde el año 1970, hemos dedicado nuestro corazón y alma a la búsqueda de los granos de café más exquisitos, llevando a nuestros clientes en una aventura de sabores que trasciende fronteras.
+              </p>
+              <p className="mx-5">
+                Nuestra historia comenzó en las laderas de las montañas de Colombia, donde descubrimos nuestra pasión por el café artesanal. Con cada grano seleccionado a mano y cada lote tostado a la perfección, nos comprometemos a ofrecer una experiencia única que solo un café de calidad superior puede proporcionar.
+              </p>
+              <p className="mx-5">
+                Te invitamos a visitarnos y ser parte de nuestra comunidad. Ya sea que prefieras un espresso intenso, un capuchino cremoso o una de nuestras especialidades de la casa, estamos aquí para servirte con una sonrisa y el mejor café que hayas probado.
+                ¡Te esperamos en Café Aromas del Mundo, donde cada sorbo cuenta una historia!
+              </p>
+            </div>
+            <div className="col-md-6">
+              <img className="img-fluid rounded-3" src={home_img} alt="Imagen home" />
+            </div>
+          </div>
+        </div>
+        </div>
   );
 };
 
